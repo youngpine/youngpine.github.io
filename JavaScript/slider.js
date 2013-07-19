d3469158184b5678811eb8d0e97f2bf60bb60a25
youngpine <1052693298@qq.com>
2013-07-19 15:15:42 +0800

    blog

--- JavaScript/slider.js
+++ JavaScript/slider.js
@@ -1,11 +1,21 @@
-window.onload=rotate;
-var thisImg=0;
-function rotate(){
-	var sImages=new Array("images/slider.jpg","images/slider1.jpg");
-	thisImg++;
-	if(thisImg==sImages.length){
-		thisImg=0;
-	}
-	document.getElementById("sliderImg").src=sImages[thisImg];
-	setTimeout(rotate,2*1000);
+window.onload=comment_initAll;
+function comment_initAll(){
+	document.getElementsByTagName("form")[1].onsubmit=comment_addNode;
+}
+function comment_addNode(){
+	var inText1=document.getElementById("comment_title").value;
+	var inText2=document.getElementById("comment_who").value;
+	var inText3=document.getElementById("comment_content").value;
+	var newText1=document.createTextNode(inText1+"_"+inText2);
+	var newText3=document.createTextNode(inText3);
+	var newGraf1=document.createElement("h4");
+	newGraf1.appendChild(newText1);
+	var newGraf=document.createElement("p");
+	newGraf.appendChild(newText3);
+	var docBody=document.getElementById("add_content");
+	docBody.appendChild(newGraf1);
+	docBody.appendChild(newGraf);
+	docBody.firstChild(newGraf1);
+	docBody.firstChild(newGraf);
+	return false;
 }
--- _includes/slider.md
+++ _includes/slider.md
@@ -1,3 +1,15 @@
+<script type="text/javascript">
+var thisImg=0;
+function rotate(){
+	var sImages=new Array("images/slider.jpg","images/slider1.jpg");
+	thisImg++;
+	if(thisImg==sImages.length){
+		thisImg=0;
+	}
+	document.getElementById("sliderImg").src=sImages[thisImg];
+	setTimeout(rotate,2*1000);
+}
+</script>
 <div id="slider">
                <img src="images/slider.jpg" id="sliderImg"width="735" height="241" />
-            </div>
+</div>
