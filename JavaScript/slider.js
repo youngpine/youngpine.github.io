095347f6f7d10b10af7e81588a4b63372241fe8e
youngpine <1052693298@qq.com>
2013-08-09 19:18:22 +0800

    blog

--- JavaScript/slider.js
+++ JavaScript/slider.js
@@ -1,40 +1,7 @@
 window.onload=colorChange;
 
 	var thisImg=0;
-function colorChange(){
-	var bgChange=document.getElementsByTagName("ul");
-	var bgChange1=document.getElementById("name");
-	var bgChange2=document.getElementById("nav")
-	for(var i=0;i<bgChange.childNodes.length;i++){
-		var bgChanges=bgChange.childNodes[i];
-		if(bgChanges.tagName == "a"){
-		   for(var j=0;j<bgChanges.childNodes.length;j++){
-		       bgChanges.childNodes[j].onmouseover=changeColorover;
-		       bgChanges.childNodes[j].onmouseout=changeColorout;
-		   }
-	    }
-	}
-	bgChange1.onmouseover=changeColorover;
-	bgChange1.onmouseout=changeColorout;
-	rotate();	
-}
-function changeColorover(){
-	if(bgChange2||bgChange1){
-	     this.style.background="#fff";
-	}
-	else{
-		this.style.background="#ebe9e9"
-	}
-} 
-function changeColorout(){
-	if(bgChange2||bgChange1){
-	    this.style.background="#f3dfba";
-	}
-	else{
-		this.style.background="#fff"
-	}
-} 
-function rotate(){
+	function rotate(){
 
 	var sImages=new Array("images/slider.jpg","images/slider1.jpg");
 	thisImg++;
@@ -44,3 +11,38 @@ function rotate(){
 	document.getElementById("sliderImg").src=sImages[thisImg];
 	setTimeout(rotate,2*1000);
 }
+function colorChange(){
+	var bgChange=document.getElementsByTagName("ul");
+	for(var k=0;k<bgChange.length;k++){
+		var bgChange2=bgChange[k];
+	    for(var i=0;i<bgChange2.childNodes.length;i++){
+		   var bgChanges=bgChange2.childNodes[i];
+		   if(bgChanges.tagName == "A"){
+		       for(var j=0;j<bgChanges.childNodes.length;j++){
+		           bgChanges.childNodes[j].onmouseover=changeColorover;
+		           bgChanges.childNodes[j].onmouseout=changeColorout;
+		       }
+	       }
+	    }
+	}
+	var bgChange1=document.getElementById("name");
+	bgChange1.onmouseover=function(){this.style.background="#fff";};
+	bgChange1.onmouseout=function(){this.style.background="#f3dfba";};
+	rotate();	
+}
+function changeColorover(){
+	if(this.parentNode.parentNode.className=="nav"){
+	     this.style.background="#fff";
+	}
+	else{
+		this.style.background="#e9e9e9";
+	}
+} 
+function changeColorout(){
+	if(this.parentNode.parentNode.className=="nav"){
+	    this.style.background="#f3dfba";
+	}
+	else{
+		this.style.background="#fff";
+	}
+} 
